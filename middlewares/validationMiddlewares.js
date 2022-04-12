09c93ef5aed65ec82cecf9f65012c582d1e6e58d
Amir <amirhatam136@gmail.com>
2022-04-12 16:50:17 +0200

    add information about the pass validation errors

---
+++ middlewares/validationMiddlewares.js
@@ -0,0 +1,34 @@
+const expressValidator = require("express-validator");
+const passwordValidator = require('password-validator');
+
+
+const validate = (req, res, next) => {
+    const errors = expressValidator.validationResult(req)
+
+    if (!errors.isEmpty()) {
+        // TODO: send the information about the validation errors
+        res.status(400).json(errors)
+    } else {
+        next()
+    }
+}
+
+const validationSignup = [
+    expressValidator.body('password').exists().isString().custom(value => {
+        const schema = new passwordValidator()
+
+        schema
+            .is().min(4)
+            .is().max(30)
+            .has().uppercase()
+            .has().lowercase()
+            .has().digits()
+            .has().not().spaces()
+            .is().not().oneOf(["Salut-12"])
+        return schema.validate(value)
+
+    }),
+    validate
+]
+
+module.exports = { validationSignup }
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,9 +1,9 @@
 const express = require("express")
 const router = express.Router()
 const { signup } = require("../controllers/authController")
+const { validationSignup } = require('../middlewares/validationMiddlewares')
 
-
-router.post("/signup", signup)
+router.post("/signup", validationSignup, signup)
 
 
 module.exports = router
